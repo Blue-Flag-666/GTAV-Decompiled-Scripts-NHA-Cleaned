16ca2f6d3c3bcf91c5c6c0e4328a08f8a997d137
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-06-20 07:08:13 +0930

    mpstatsinit.c Uploaded @ 6/19/2023 9:38:13 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.67 Build 2944/mpstatsinit.c
@@ -0,0 +1,41 @@
+//Cleaned With dr NHA's C Script Cleaner V2.8
+
+
+void __EntryFunction__(){
+func_1();
+Global_2694509=1;
+SCRIPT::TERMINATE_THIS_THREAD();
+}
+
+
+void func_1(){
+Global_1665476[0]=joaat("mpply_awd_fm_cr_races_made");
+Global_1665476[1]=joaat("mpply_awd_fm_cr_dm_made");
+Global_1665476[2]=joaat("mpply_awd_fm_cr_played_by_peep");
+Global_1665476[3]=joaat("mpply_awd_fm_cr_mission_score");
+Global_1665476[4]=joaat("mpply_awd_tempaward");
+Global_1665482[0]=joaat("mpply_awd_tempawardfloat");
+Global_1665484[0]=joaat("mpply_awd_tempawardbool");
+Global_1665484[1]=joaat("mpply_awd_gangops_iaa");
+Global_1665484[2]=joaat("mpply_awd_gangops_submarine");
+Global_1665484[3]=joaat("mpply_awd_gangops_allinorder");
+Global_1665484[4]=joaat("mpply_awd_gangops_loyalty");
+Global_1665484[5]=joaat("mpply_awd_gangops_crimmasmd");
+Global_1665484[6]=joaat("mpply_awd_gangops_missile");
+Global_1665484[7]=joaat("mpply_awd_gangops_loyalty2");
+Global_1665484[8]=joaat("mpply_awd_gangops_loyalty3");
+Global_1665484[9]=joaat("mpply_awd_gangops_crimmasmd2");
+Global_1665484[10]=joaat("mpply_awd_gangops_crimmasmd3");
+Global_1665484[11]=joaat("mpply_awd_gangops_support");
+Global_1665484[12]=joaat("mpply_awd_fleeca_fin");
+Global_1665484[13]=joaat("mpply_awd_prison_fin");
+Global_1665484[14]=joaat("mpply_awd_humane_fin");
+Global_1665484[15]=joaat("mpply_awd_seriesa_fin");
+Global_1665484[16]=joaat("mpply_awd_pacific_fin");
+Global_1665484[17]=joaat("mpply_awd_hst_order");
+Global_1665484[18]=joaat("mpply_awd_hst_same_team");
+Global_1665484[19]=joaat("mpply_awd_hst_ult_chal");
+Global_1665484[20]=joaat("mpply_awd_complet_heist_1stper");
+Global_1665484[21]=joaat("mpply_awd_complet_heist_mem");
+Global_1665507[0]=joaat("mpply_awd_tempawarddate");
+}
