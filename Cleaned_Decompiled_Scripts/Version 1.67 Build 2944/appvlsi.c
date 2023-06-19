6cf32d06d407762560034eeb9295b2df4814ec3b
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-06-20 05:11:31 +0930

    appvlsi.c Uploaded @ 6/19/2023 7:41:31 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.67 Build 2944/appvlsi.c
@@ -0,0 +1,256 @@
+//Cleaned With dr NHA's C Script Cleaner V2.8
+#region Local Var
+var uLocal_0=0;
+var uLocal_1=0;
+int iLocal_2=0;
+int iLocal_3=0;
+int iLocal_4=0;
+int iLocal_5=0;
+int iLocal_6=0;
+int iLocal_7=0;
+int iLocal_8=0;
+int iLocal_9=0;
+int iLocal_10=0;
+int iLocal_11=0;
+float fLocal_12=0f;
+var uLocal_13=0;
+var uLocal_14=0;
+int iLocal_15=0;
+var uLocal_16=0;
+var uLocal_17=0;
+int iLocal_18=0;
+int iLocal_19=0;
+#endregion
+
+void __EntryFunction__(){
+iLocal_2=1;
+iLocal_3=134;
+iLocal_4=134;
+iLocal_5=1;
+iLocal_6=1;
+iLocal_7=1;
+iLocal_8=134;
+iLocal_9=1;
+iLocal_10=12;
+iLocal_11=12;
+fLocal_12=0.001f;
+iLocal_15=-1;
+MISC::NETWORK_SET_SCRIPT_IS_SAFE_FOR_NETWORK_GAME();
+iLocal_19=0;
+if(iLocal_18==0){
+func_10();
+func_9();
+iLocal_18=1;
+}
+while (true){
+wait(0);
+if(Global_20500.f_1 !=9){
+switch (Global_20500.f_1){
+case 7:
+if(iLocal_19==1){
+if(func_8(2, Global_20469, 0)){
+AUDIO::PLAY_SOUND_FRONTEND(-1, "Lester_Laugh_Phone", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1);
+Global_20478=1;
+func_10();
+func_5();
+settimera(0);
+settimerb(0);
+}}
+if(iLocal_19==2){
+if(timerb() > 499){
+settimerb(0);
+}
+if(timera() > 2200){
+iLocal_19=3;
+}}
+if(iLocal_19==3){
+Global_1836105=1;
+}
+break;
+case 8:
+if(func_8(2, Global_20468, 0)){
+func_4();
+Global_20478=1;
+if(Global_20500.f_1 > 3){
+Global_20500.f_1=7;
+}}
+break;
+default:
+break;
+}
+if(func_3()){
+func_2();
+}}else{
+Global_20502=6;
+func_2();
+}
+if(func_1()){
+func_2();
+}}}
+
+int func_1(){
+if(((Global_20500.f_1==1 || Global_20500.f_1==3) || Global_20500.f_1==0) || Global_20444==1){
+Global_20487=1;
+return 1;
+}
+return 0;
+}
+
+
+void func_2(){
+AUDIO::RELEASE_NAMED_SCRIPT_AUDIO_BANK("DLC_MPHEIST/HEIST_HACK_SNAKE");
+SCRIPT::TERMINATE_THIS_THREAD();
+}
+
+int func_3(){
+if(Global_8976==1 || Global_20500.f_1 < 7){
+Global_20487=1;
+return 1;
+}
+return 0;
+}
+
+
+void func_4(){
+if(!ENTITY::IS_ENTITY_DEAD(PLAYER::PLAYER_PED_ID(), 0)){
+AUDIO::PLAY_SOUND_FRONTEND(-1, "Menu_Back", &Global_20489, 1);
+}}
+
+
+void func_5(){
+if(Global_20488){
+func_6(Global_20481, "SET_SOFT_KEYS", 2f, 0f, 13f, -1f, -1f, "CELL_201", 0, 0, 0, 0);
+func_6(Global_20481, "SET_SOFT_KEYS", 3f, 1f, 14f, -1f, -1f, "CELL_213", 0, 0, 0, 0);
+}else{
+func_6(Global_20481, "SET_SOFT_KEYS", 2f, 0f, 13f, -1f, -1f, 0, 0, 0, 0, 0);
+func_6(Global_20481, "SET_SOFT_KEYS", 3f, 1f, 14f, -1f, -1f, 0, 0, 0, 0, 0);
+}
+func_6(Global_20481, "SET_SOFT_KEYS", 1f, 0f, 1f, -1f, -1f, 0, 0, 0, 0, 0);
+MISC::CLEAR_BIT(&Global_8370, 17);
+}
+
+
+void func_6(var uParam0, char* sParam1, float fParam2, float fParam3, float fParam4, float fParam5, float fParam6, char* sParam7, int iParam8, int iParam9, int iParam10, int iParam11){
+GRAPHICS::BEGIN_SCALEFORM_MOVIE_METHOD(uParam0, sParam1);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam2));
+if(fParam3 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam3));
+}
+if(fParam4 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam4));
+}
+if(fParam5 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam5));
+}
+if(fParam6 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam6));
+}
+if(!MISC::IS_STRING_NULL_OR_EMPTY(sParam7)){
+func_7(sParam7);
+}
+if(!MISC::IS_STRING_NULL_OR_EMPTY(iParam8)){
+func_7(iParam8);
+}
+if(!MISC::IS_STRING_NULL_OR_EMPTY(iParam9)){
+func_7(iParam9);
+}
+if(!MISC::IS_STRING_NULL_OR_EMPTY(iParam10)){
+func_7(iParam10);
+}
+if(!MISC::IS_STRING_NULL_OR_EMPTY(iParam11)){
+func_7(iParam11);
+}
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
+
+
+void func_7(var uParam0){
+GRAPHICS::BEGIN_TEXT_COMMAND_SCALEFORM_STRING(uParam0);
+GRAPHICS::END_TEXT_COMMAND_SCALEFORM_STRING();
+}
+
+int func_8(int iParam0, var uParam1, int iParam2){
+if(PAD::IS_CONTROL_JUST_PRESSED(iParam0, uParam1) || (iParam2==1 && PAD::IS_DISABLED_CONTROL_JUST_PRESSED(iParam0, uParam1))){
+if(MISC::IS_PC_VERSION()){
+if(MISC::UPDATE_ONSCREEN_KEYBOARD()==0 || (NETWORK::NETWORK_TEXT_CHAT_IS_TYPING() && PAD::IS_USING_KEYBOARD_AND_MOUSE(2))){
+return 0;
+}}
+if(HUD::IS_PAUSE_MENU_ACTIVE() || HUD::IS_WARNING_MESSAGE_ACTIVE()){
+return 0;
+}else{
+return 1;
+}}
+return 0;
+}
+
+
+void func_9(){
+if(Global_20488){
+func_6(Global_20481, "SET_SOFT_KEYS", 2f, 1f, 13f, -1f, -1f, "CELL_201", 0, 0, 0, 0);
+func_6(Global_20481, "SET_SOFT_KEYS", 3f, 1f, 14f, -1f, -1f, "CELL_213", 0, 0, 0, 0);
+}else{
+func_6(Global_20481, "SET_SOFT_KEYS", 2f, 1f, 13f, -1f, -1f, 0, 0, 0, 0, 0);
+func_6(Global_20481, "SET_SOFT_KEYS", 3f, 1f, 14f, -1f, -1f, 0, 0, 0, 0, 0);
+}
+func_6(Global_20481, "SET_SOFT_KEYS", 1f, 0f, 1f, -1f, -1f, 0, 0, 0, 0, 0);
+MISC::CLEAR_BIT(&Global_8370, 17);
+}
+
+
+void func_10(){
+if(iLocal_19==0){
+AUDIO::REQUEST_SCRIPT_AUDIO_BANK("DLC_MPHEIST/HEIST_HACK_SNAKE", 0, -1);
+while (AUDIO::REQUEST_SCRIPT_AUDIO_BANK("DLC_MPHEIST/HEIST_HACK_SNAKE", 0, -1)==0){
+wait(0);
+}
+func_11(Global_20481, "SET_DATA_SLOT_EMPTY", 23f, -1082130432, -1082130432, -1082130432, -1082130432);
+GRAPHICS::BEGIN_SCALEFORM_MOVIE_METHOD(Global_20481, "SET_DATA_SLOT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(23);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(0);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(-99);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(0);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(100);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(2);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_BOOL(0);
+if(MISC::IS_BIT_SET(Global_8372, 1)){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_FLOAT(0f);
+}
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+func_11(Global_20481, "DISPLAY_VIEW", 23f, to_float(1), -1082130432, -1082130432, -1082130432);
+iLocal_19=1;
+}elseif(iLocal_19==1){
+func_11(Global_20481, "SET_DATA_SLOT_EMPTY", 23f, -1082130432, -1082130432, -1082130432, -1082130432);
+GRAPHICS::BEGIN_SCALEFORM_MOVIE_METHOD(Global_20481, "SET_DATA_SLOT");
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(23);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(0);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(-99);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(0);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(100);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(3);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_BOOL(0);
+if(MISC::IS_BIT_SET(Global_8372, 1)){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_FLOAT(0f);
+}
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+func_11(Global_20481, "DISPLAY_VIEW", 23f, to_float(1), -1082130432, -1082130432, -1082130432);
+iLocal_19=2;
+}}
+
+
+void func_11(var uParam0, char* sParam1, float fParam2, float fParam3, float fParam4, float fParam5, float fParam6){
+GRAPHICS::BEGIN_SCALEFORM_MOVIE_METHOD(uParam0, sParam1);
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam2));
+if(fParam3 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam3));
+}
+if(fParam4 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam4));
+}
+if(fParam5 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam5));
+}
+if(fParam6 !=-1f){
+GRAPHICS::SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(round(fParam6));
+}
+GRAPHICS::END_SCALEFORM_MOVIE_METHOD();
+}
