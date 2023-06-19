0645a48e9aa507bf3922a012276dcba222ac63fa
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2023-06-20 07:29:59 +0930

    ugc_global_registration_2.c Uploaded @ 6/19/2023 9:59:59 PM

---
+++ Cleaned_Decompiled_Scripts/Version 1.67 Build 2944/ugc_global_registration_2.c
@@ -0,0 +1,17 @@
+//Cleaned With dr NHA's C Script Cleaner V2.8
+
+
+void __EntryFunction__(){
+Global_1057409=-1;
+Global_1058373={
+10.61f, 0.11f, 210.2f 
+};
+Global_1058376={
+0f, 0f, 0f 
+};
+Global_1058379=32;
+Global_1058380=5000;
+Global_1058381=1;
+Global_1058382=-1;
+Global_1058383=1;
+}
